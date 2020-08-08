e8fc6dec97b7519ae215ff1cd47bfabc551bad4e
Damilola <immanueladeniyi7@gmail.com>
2020-08-08 02:39:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var toggleStatus = 0;
+var button1 = document.getElementById('navButton__button1');
+var button2 = document.getElementById('navButton__button2');
+var first = jQuery(".first");
+var second = jQuery(".second");
+
+function toggleMenu(){
+    if(toggleStatus == 0){
+        button1.style.display = 'none';
+        button2.style.display = 'block';
+        toggleStatus = 1;
+    }else if(toggleStatus == 1){
+        button2.style.display = 'none';
+        button1.style.display = 'block';
+        toggleStatus = 0;
+    };
+};
